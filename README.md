da38034b283a680b4c9361054f7f643ef6b32b13
Fred <fredericobnm@gmail.com>
2018-02-07 00:28:01 -0200

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Startyourapp
+
+A simple template for Angular 5 with initial routes service work configured
+
+
+## Let's start
+
+clone this project
+
+run the `npm install` inside the cloned directory
+
+## Running the application
+
+Run `ng serve` to view app 
+
+### Thanks
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { AppRoutingModule } from './app.routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
